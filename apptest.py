00f73e45698e1fc6f94c296504bae7c33187067d
tomaskurten <tomas.kurten@bue.edu.ar>
2024-06-25 18:00:09 -0300

    create read delete

--- apptest.py
+++ apptest.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
 from flaskext.mysql import MySQL
+from flask import render_template, request, redirect
 
 app=Flask(__name__)
 
@@ -20,13 +20,56 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    # return "<h1> HOLA MUNDO!</h1> <p>Adios!</p>"
-    sql="INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Bartolo', 'bartolo@mail.com', 'bartolouser.jpg');"
+    sql="SELECT * FROM `sistema`.`empleados`"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
+    db_empleados=cursor.fetchall()
+    for empleado in db_empleados:
+        print(empleado)
+    # conn.commit()
+    return render_template("empleados/index.html",empleados=db_empleados)
+
+@app.route("/create")
+def create():
+    return render_template("empleados/create.html")
+
+@app.route("/store",methods=["POST"])
+def storage():
+    _nombre=request.form["txtNombre"]
+    _correo=request.form["txtCorreo"]
+    _foto="null.jpg"
+    
+    datos=(_nombre,_correo,_foto)
+    
+    sql="INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
     conn.commit()
-    return render_template("empleados/index.html")
+    return redirect('/')
+
+@app.route("/destroy/<int:id>")
+def destroy(id):
+    sql="DELETE FROM `sistema`.`empleados` WHERE `empleados`.`id` = %s"
+    
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,(id))
+    conn.commit()
+    return redirect('/')
+
+@app.route("/edit/<int:id>")
+def edit(id):
+    # sql="UPDATE `empleados` SET `nombre` = 'editexitoso' WHERE `empleados`.`id` = 12;"
+    sql="SELECT * FROM `sistema`.`empleados` WHERE id=12"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    empleados=cursor.fetchall()
+    
+    conn.commit()
+    return render_template("empleados/edit.html")
 
 if __name__=="__main__":
     app.run(debug=True,port=8055)
